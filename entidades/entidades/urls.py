f6de19ef67b15b7469affd515ca6aacd2ec44d35
hernandavidc <hernandavidc@hotmail.com>
2019-09-14 17:44:14 -0500

    Api llamado de todos los asist y por id

--- entidades/entidades/urls.py
+++ entidades/entidades/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from instituciones.views import getUsuarios, getUsuarioDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("todos", getUsuarios),
+    path("estudiante/<int:pk>/", getUsuarioDetail)
 ]
--- entidades/instituciones/views.py
+++ entidades/instituciones/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Inscrito
+from django.http import JsonResponse
 
-# Create your views here.
+def getUsuarios(request):
+    users = Inscrito.objects.all()
+    data = list(users.values("id","nombre"))
+    return JsonResponse(data, safe=False)
+
+def getUsuarioDetail(request, pk):
+    data = Inscrito.objects.filter(id=pk)
+    return JsonResponse(list(data.values("id","nombre", "programas")),safe=False)
