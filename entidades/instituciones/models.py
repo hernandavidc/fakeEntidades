2e25b7fe886809b2a479523c19d43c9d37a8ddd5
hernandavidc <hernandavidc@hotmail.com>
2019-09-17 12:17:14 -0500

    Json para cargar datos basicos
    UPB + Sistemas + Andrea/Hernan/Luis
    Comandos:
    python manage.py dumpdata instituciones > db.json
    python manage.py loaddata db.json

--- entidades/instituciones/models.py
+++ entidades/instituciones/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.utils.text import slugify 
 
 class Institucion(models.Model):
-    nombre = models.CharField(max_length=150, primary_key=True)
-    s = models.CharField(max_length=160)
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=150, unique=True)
+    s = models.SlugField(max_length=160,default=None, blank=True)
 
     def save(self, *args, **kwargs):
-        self.s = slugify(self.nombre)
+        self.s = slugify(self.nombre, allow_unicode=True)
         super(Institucion, self).save(*args, **kwargs)
 
 class Programa(models.Model):
@@ -13,6 +15,7 @@ class Programa(models.Model):
     institucion = models.ForeignKey(Institucion, related_name="get_programas", on_delete=models.PROTECT)
 
 class Inscrito(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=150)
     institucion = models.ForeignKey(Institucion, related_name="get_inscritos", on_delete=models.PROTECT)
     programas = models.ManyToManyField(Programa)
